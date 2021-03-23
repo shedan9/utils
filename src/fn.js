2f362c0dc6267b968f16b740a6364612d7dfb08c
shedan <shedan@zenki.ai>
2021-03-23 14:41:28 +0800

    ie version

--- src/fn.js
+++ src/fn.js
@@ -298,3 +298,36 @@ export function cacheDecorator(func) {
     return result;
   };
 }
+
+// 获取浏览器IE版本：6，7，8，9，10，11，'edge'，-1
+export function IEVersion() {
+  var userAgent = navigator.userAgent;
+  var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1;
+  var isEdge = userAgent.indexOf("Edge") > -1 && !isIE;
+  var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+
+  if (isIE) {
+    var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+    reIE.test(userAgent);
+    var fIEVersion = parseFloat(RegExp["$1"]);
+
+    if (fIEVersion == 7) {
+      return 7;
+    } else if (fIEVersion == 8) {
+      return 8;
+    } else if (fIEVersion == 9) {
+      return 9;
+    } else if (fIEVersion == 10) {
+      return 10;
+    } else {
+      return 6;
+    }
+
+  } else if (isEdge) {
+    return 'edge';
+  } else if (isIE11) {
+    return 11;
+  } else {
+    return -1;
+  }
+}
